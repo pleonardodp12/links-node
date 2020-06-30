724849389ad335a6a816d7d16bc0f2fd84401d7c
Leonardo Dias <pauloleonardo_dp@outlook.com>
2020-06-30 15:08:13 -0300

    nodemon config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const authController = require('./controllers/auth')
 
 const app = express();
+app.use('/auth', authController)
 
 
 app.get('/', (req, res) => {
