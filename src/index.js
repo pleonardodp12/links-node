d11a2806f95eb4c25ec8ff8fc3ef86d7b4826b48
Leonardo Dias <pauloleonardo_dp@outlook.com>
2020-06-30 23:23:14 -0300

    dotenv configurado

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const db = require('./models');
+
 const authController = require('./controllers/auth')
 
 const app = express();
 app.use('/auth', authController)
 
-
 app.get('/', (req, res) => {
     return res.json('Api rodando. . .')
 })
 
-app.listen(3001, () => {
-    console.log("http://localhost:3001/")
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("http://localhost:3001/")
+    });
 });
+
